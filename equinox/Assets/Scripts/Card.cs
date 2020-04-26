38fde856d1ec5d24b741e6b48da180105d4f0d32
Jeko <52203544+JekoDev@users.noreply.github.com>
2020-04-26 16:01:25 +0200

    [Prototype]
    Added Iso Playground
    Menu
    Other Stuff
    Card Template as Scriptable Object

---
+++ equinox/Assets/Scripts/Card.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Equinox/Card", order = 1)]
+public class Card : ScriptableObject
+{
+    public string CardName;
+    public Sprite CardImage;
+    public string Meaning1;
+    public string Meaning2;
+    public string Meaning3;
+}
---
+++ equinox/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public static void MenuStart() {
+        SceneManager.LoadScene(1);
+    }
+
+    public static void MenuEnd() {
+        // save any game data here
+        #if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                 Application.Quit();
+        #endif
+    }
+}
